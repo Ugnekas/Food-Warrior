a793f8eff5efbc9134d4568240ccffee2001e3fb
NIEKAS <ugnius.silinskas@vilnius.eu>
2024-01-31 19:26:51 +0200

    Slicer

---
+++ Assets/Scripts/Slicer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slicer : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
+        rb.MovePosition(worldPos);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(":)");
+        Destroy(collision.gameObject);
+    }
+}
