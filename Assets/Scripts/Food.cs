050834ff6488072449e3f0e78603f7dc93712f1e
NIEKAS <ugnius.silinskas@vilnius.eu>
2024-01-31 18:59:15 +0200

    Fruit

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -6)
+        {
+            print(":(");
+            Destroy(gameObject);
+        }
+    }
+}
